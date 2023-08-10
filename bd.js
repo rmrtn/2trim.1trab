467bf8f35362acb2d4e1dd2293d70f95d2b657e1
Raíssa Martins Nascimento <132698060+rmrtn@users.noreply.github.com>
2023-08-10 11:48:53 +0000

    changes

--- bd.js
+++ bd.js
@@ -12,4 +12,17 @@ async function connect() {
     const res = await client.query("SELECT * FROM usuario");
     return res.rows;
   }
-  export { selectUsuarios };
+export { selectUsuarios, selectUsuario, insertUsuario };
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios } from "./bd.js";
-
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 dotenv.config();
 
 const app = express(); // Instancia o Express
@@ -31,4 +30,25 @@ app.get("/usuarios", async (req, res) => {
 app.listen(port, () => {
   // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+app.use(express.json());
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
